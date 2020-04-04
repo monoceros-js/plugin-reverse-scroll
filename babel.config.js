d5be9048eb0351db4eb8e7fddd98721efb03c3cb
Folkert-Jan van der Pol <folkertjanvanderpol@gmail.com>
2020-04-04 23:03:50 +0200

    Removed polyfill forcing

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,16 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true)
 
   const presets = []
-  if (process.env.BUILD === 'browser_production') {
-    console.log('BUILDING BROWSER PRODUCTION')
+  if (process.env.BUILD === 'browser') {
+    console.log('BUILDING BROWSER')
     presets.push([
       '@babel/env',
       {
         targets: '> 0.25%, not dead',
         modules: false,
-        useBuiltIns: 'usage',
+        useBuiltIns: 'entry',
         corejs: 3,
-      },
-    ])
-  } else if (process.env.BUILD === 'browser_development') {
-    console.log('BUILDING BROWSER DEVELOPMENT')
-    presets.push([
-      '@babel/env',
-      {
-        targets: '> 0.25%, not dead',
-        modules: false,
       },
     ])
   } else {
